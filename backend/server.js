a4a68bbca3a6b236ea0647677dda6ed407f007fb
vishal14marathe <vishalmarathe@gmail.com>
2025-04-27 17:03:35 +0530

    your messages

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
